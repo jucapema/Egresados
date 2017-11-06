51bdf4e3a49606f239d30cb847c0e0f2bfa8246a
camilo <camilo89@utp.edu.co>
2017-11-05 22:32:59 -0500

    configurando bd

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        //Illuminate\Support\Schema::class,
 
         /*
          * Package Service Providers...
