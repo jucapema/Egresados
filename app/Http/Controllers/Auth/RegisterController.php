cfefd10ee829f56240e26feba5d40cd3ca2bb894
camilo <camilo89@utp.edu.co>
2017-11-08 15:02:59 -0500

    modificaciones de fill user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    //protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -51,6 +51,7 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'apellido' => 'required|string|max:255',
         ]);
     }
 
@@ -62,10 +63,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'apellido' => $data['apellido'],
+            'tipo_rol'=>'egresado',
+            'estado_cuenta'=>'suscrita',
         ]);
     }
 }
