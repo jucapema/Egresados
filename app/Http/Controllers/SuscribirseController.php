ce39b80387cc214f32f3f304b27e6d0c73adcdd8
camilo <camilo89@utp.edu.co>
2017-11-12 20:49:06 -0500

    suscripcion funcional 100

--- app/Http/Controllers/SuscribirseController.php
+++ app/Http/Controllers/SuscribirseController.php
@@ -1,25 +1,46 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
+use App\User;
 use Illuminate\Http\Request;
-
+use Carbon\Carbon;
+use Auth;
+use Session;
 class SuscribirseController extends Controller
 {
 
     public function store(Request $request)
     {
-      User::create([
-         'name' => $request['name'],
-         'email' => $request['email'],
-         'password' => bcrypt('egresado123456'),
-         'apellido' => $request['apellido'],
-         'tipo_rol'=>'egresado',
-         'estado_cuenta'=>'suscrita',
-     ]);
-     //crear mensaje
-     return view('auth.login');
-     //redirect()->view('/welcome');
-    }
-
+      $v = \Validator::make($request->all(), [
+            'name' => 'required',
+            'dni' => 'required|unique:users',
+            'email'    => 'required|email|unique:users',
+        ]);
+        if ($v->fails())
+        {
+          return redirect()->back()->withInput()->withErrors($v->errors());
+        }
+        else{
+          $edad = Carbon::parse($request['fecha_nacimiento'])->age;
+          if($edad>=18){
+            User::create([
+               'name' => $request['name'],
+               'dni' => $request['dni'],
+               'email' => $request['email'],
+               'apellido' => $request['apellido'],
+               'password' => $request['password'],
+               'password_confirmation' => $request['password_confirmation'],
+               'tipo_rol'=>'egresado',
+               'estado_cuenta'=>'suscrita',
+             ]);
+             Auth::logout();
+             Session::flash('flash_message', 'Solicitud en Proceso');
+             return view('auth.login');
+          }
+          else{
+            Session::flash('flash_message', 'Debes ser mayor de edad');
+             return view('auth.login');
+          }
+      }
+  }
 }
